284be50fe40f742d22a59126edb19dbf9cd57b3f
kellenThaisaLima <kellen1393@gmail.com>
2021-11-28 12:46:47 -0300

    Corrigindo Procfile src/servidor.js5

---
+++ src/server.js
@@ -0,0 +1,16 @@
+const Express = require('express')
+const cors = require('cors')
+const bd =require('./data/sqlite-bd')
+const imigranteController = require('./controllers/imigrante-controller')
+const parceiroController = require('./controllers/parceiro-controller')
+const port = 3030
+const app = Express()
+
+
+app.use(cors())
+app.use(Express.json())
+
+imigranteController(app, bd)
+parceiroController(app, bd)
+
+app.listen(process.env.PORT||port, ()=>console.log(`[INFO]Servidor rodando na porta: ${port}`))
